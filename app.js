4e57043642f98352095a5e1b14f641630197829b
knightShas <kumarshashank45@gmail.com>
2022-10-17 21:24:27 +0530

    Global variable

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const amount = 12
 
+// var name = "Snasj"
+
 if(amount < 10){
     console.log('small number')
 }
@@ -7,4 +9,19 @@ else{
     console.log('large number')
 }
 
+// Global variables
+
+// __dirname = path to the current directory
+// __filename = file name
+// require = function to use modules(CommonJS)
+// module = info about current module (files)
+// process = info about env where the program is being executed
+
+
+console.log(__dirname,__filename)
+
 console.log(`Hello world shashank!!`)
+
+setInterval(() => {
+    console.log('Lenovo');
+},1000)
